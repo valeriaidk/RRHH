eaa58f9659759c783d92046b65cc86c1fa2aad56
valeriaidk <valeriaquezada6@hotmail.com>
2025-04-24 18:30:20 -0500

    24/04

--- src/main/java/senati/rrhh/controlador/ClienteControlador.java
+++ src/main/java/senati/rrhh/controlador/ClienteControlador.java
@@ -3,14 +3,15 @@ package senati.rrhh.controlador;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import senati.rrhh.modelo.Cliente;
+import senati.rrhh.modelo.Empleado;
 import senati.rrhh.servicio.IClienteServicio;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -25,11 +26,45 @@ public class ClienteControlador {
     @Autowired
     private IClienteServicio clienteServicio;
 
-    //http://localhost:8080/rrhh-app/clientes
+    //http://localhost:8080/rrhh-app/cliente
     @GetMapping("/cliente")
     public List<Cliente> obtenerclientes(){
         var clientes = clienteServicio.ListarClientes();
         clientes.forEach(Cliente -> logger.info(clientes.toString()));
         return clientes;
+    }
+
+    @PostMapping("/cliente")
+    public Cliente agregarCliente(@RequestBody Cliente cliente){
+        return clienteServicio.guardarCliente(cliente);
+    }
+
+    @GetMapping("/cliente/{id}")
+    public ResponseEntity<Cliente> buscarClienteId (@PathVariable Integer id){
+        Cliente cliente = clienteServicio.buscarClientePorID(id);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping("cliente/{id}")
+    public ResponseEntity<Cliente> actualizarCliente(@PathVariable Integer id,
+                                                       @RequestBody Cliente clienteUpdate){
+        Cliente cliente = clienteServicio.buscarClientePorID(id);
+        cliente.setApellido(clienteUpdate.getApellido());
+        cliente.setNombre(clienteUpdate.getNombre());
+        cliente.setTelefono(clienteUpdate.getTelefono());
+        cliente.setDireccion(clienteUpdate.getDireccion());
+        cliente.setEmail(clienteUpdate.getEmail());
+
+        clienteServicio.guardarCliente(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+    @DeleteMapping("cliente/{id}")
+    public ResponseEntity<Map<String, Boolean>> eliminarCliente(@PathVariable Integer id){
+        Cliente cliente = clienteServicio.buscarClientePorID(id);
+        clienteServicio.eliminarCliente(cliente);
+        //imprimir mesanje de confirmacion
+        Map<String, Boolean> respuesta = new HashMap<>();
+        respuesta.put("cliente Eliminado", Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
     }
 }
--- src/main/java/senati/rrhh/controlador/EmpleadoControlador.java
+++ src/main/java/senati/rrhh/controlador/EmpleadoControlador.java
@@ -3,14 +3,14 @@ package senati.rrhh.controlador;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import senati.rrhh.modelo.Empleado;
 import senati.rrhh.servicio.IEmpleadoServicio;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @RestController
@@ -31,5 +31,40 @@ public class EmpleadoControlador {
         var empleados = empleadoServicio.ListarEmpleados();
         empleados.forEach(empleado -> logger.info(empleado.toString()));
         return empleados;
+    }
+    //agregar es un post
+    @PostMapping("/empleados")
+    public Empleado agregarEmpleado(@RequestBody Empleado empleado){
+        return empleadoServicio.guardarEmpleado(empleado);
+    }
+    @GetMapping("/empleados/{id}")
+    public ResponseEntity<Empleado> buscarEmpleadoId (@PathVariable Integer id){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorID(id);
+        return ResponseEntity.ok(empleado);
+    }
+    @PutMapping("empleados/{id}")
+    public ResponseEntity<Empleado> actualizarEmpleado(@PathVariable Integer id,
+                                                       @RequestBody Empleado empleadoUpdate){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorID(id);
+        empleado.setApellido(empleadoUpdate.getApellido());
+        empleado.setNombre(empleadoUpdate.getNombre());
+        empleado.setArea(empleadoUpdate.getArea());
+        empleado.setEmail(empleado.getEmail());
+        empleado.setSueldo(empleadoUpdate.getSueldo());
+        empleado.setDireccion(empleadoUpdate.getDireccion());
+        empleado.setTelefono(empleadoUpdate.getTelefono());
+
+        empleadoServicio.guardarEmpleado(empleado);
+        return ResponseEntity.ok(empleado);
+    }
+
+    @DeleteMapping("empleados/{id}")
+    public ResponseEntity<Map<String, Boolean>> eliminarEmpleado(@PathVariable Integer id){
+        Empleado empleado = empleadoServicio.buscarEmpleadoPorID(id);
+        empleadoServicio.eliminarEmpleado(empleado);
+        //imprimir mesanje de confirmacion
+        Map<String, Boolean> respuesta = new HashMap<>();
+        respuesta.put("empleado Eliminado", Boolean.TRUE);
+        return ResponseEntity.ok(respuesta);
     }
 }
